86a8fb46d5789589583c5c3b7c40938fda0a44e1
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-09-25 15:53:35 +0200

    added buffer based ConstantTimeAreEqual

--- KDCryptoUtils/Crypto/CryptoUtils.cs
+++ KDCryptoUtils/Crypto/CryptoUtils.cs
@@ -2,14 +2,16 @@ namespace KDCryptoUtils.Crypto
 {
   public static class CryptoUtils
   {
-    public static bool ConstantTimeAreEqual(byte[] a, byte[] b)
+    public static bool ConstantTimeAreEqual(byte[] a, byte[] b) => ConstantTimeAreEqual(a, 0, a.Length, b, 0, b.Length);
+
+    public static bool ConstantTimeAreEqual(byte[] a, int aOffset, int aLength, byte[] b, int bOffset, int bLength)
     {
-      if (a.Length != b.Length)
+      if (aLength != bLength)
         return false;
 
       int cmp = 0;
-      for (int i = 0; i < a.Length; i++)
-        cmp |= a[i] ^ b[i];
+      for (int i = 0; i < aLength; i++)
+        cmp |= a[aOffset + i] ^ b[bOffset + i];
       return cmp == 0;
     }
   }
