f2ff7b7819ec8cc9bbb17e94064b3f9a989c92eb
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-12-13 16:37:33 +0100

    Signer: moved core methods to BaseSigner.Core.cs

--- KDCryptoUtils/Signer/BaseSigner.cs
+++ KDCryptoUtils/Signer/BaseSigner.cs
@@ -1,59 +1,16 @@
 using System;
-using System.Security.Cryptography;
-using System.Text;
 using KDLib;
 
 namespace KDCryptoUtils.Signer
 {
-  public abstract class BaseSigner<T>
+  public abstract partial class BaseSigner<T>
   {
-    private int _signatureLength;
-
-    private HMACSHA1 Hmac { get; }
-    private byte[] SecretKey { get; }
-
-    public BaseSigner(string secretKey, int signatureLength = -1)
-    {
-      _signatureLength = signatureLength;
-
-      SecretKey = Encoding.ASCII.GetBytes(secretKey);
-      Hmac = new HMACSHA1(SecretKey);
-    }
-
     public string GetSignatureString(T value) => Convert.ToBase64String(GetSignatureBytes(value));
 
     public byte[] GetSignatureBytes(T value)
     {
       byte[] valueBytes = ConvertToBytes(value);
       return GetSignatureBytes(valueBytes, 0, valueBytes.Length);
-    }
-
-    public byte[] GetSignatureBytes(byte[] buffer, int offset, int count)
-    {
-      var signatureBytes = Hmac.ComputeHash(buffer, offset, count);
-      if (_signatureLength == -1) {
-        return signatureBytes;
-      }
-      else {
-        var bytesSlice = new byte[_signatureLength];
-        Array.Copy(signatureBytes, bytesSlice, _signatureLength);
-        return bytesSlice;
-      }
-    }
-
-    public string Sign(T value, BinaryEncoding encoding = BinaryEncoding.Base64)
-    {
-      byte[] valueBytes = ConvertToBytes(value);
-      var signatureBytes = GetSignatureBytes(valueBytes, 0, valueBytes.Length);
-      return CreateSignedString(valueBytes, signatureBytes, encoding);
-    }
-
-    public T Decode(string signedString, BinaryEncoding encoding = BinaryEncoding.Base64)
-    {
-      if (!ValidateInternal(signedString, encoding, out var valueBytes))
-        throw new BadSignatureException();
-
-      return ConvertFromBytes(valueBytes);
     }
 
     public void ValidateSignedString(string signedString, BinaryEncoding encoding = BinaryEncoding.Base64)
@@ -101,48 +58,5 @@ namespace KDCryptoUtils.Signer
     {
       return IsSignatureValid(valueBuffer, 0, valueBuffer.Length, signatureBytes, 0, signatureBytes.Length);
     }
-
-    protected bool IsSignatureValid(byte[] valueBuffer, int valueOffset, int valueLength, byte[] signatureBuffer, int signatureOffset, int signatureLength)
-    {
-      var desiredBytes = GetSignatureBytes(valueBuffer, valueOffset, valueLength);
-      return CryptoUtils.ConstantTimeAreEqual(desiredBytes, 0, desiredBytes.Length, signatureBuffer, signatureOffset, signatureLength);
-    }
-
-    // Helpers
-    private bool ValidateInternal(string signedString, BinaryEncoding encoding, out byte[] valueBytes)
-    {
-      if (!TryParseSignedString(signedString, encoding, out valueBytes, out var signatureBytes))
-        return false;
-
-      return IsSignatureValid(valueBytes, signatureBytes);
-    }
-
-    private static string CreateSignedString(byte[] valueBytes, byte[] signatureBytes, BinaryEncoding encoding)
-    {
-      string valueB64 = BinaryEncoder.Encode(valueBytes, encoding);
-      string signatureB64 = BinaryEncoder.Encode(signatureBytes, encoding);
-      return $"{valueB64}.{signatureB64}";
-    }
-
-    private static bool TryParseSignedString(string signedString, BinaryEncoding encoding, out byte[] valueBytes, out byte[] signatureBytes)
-    {
-      var parts = signedString.Split(new[] { '.' }, 2);
-      if (parts.Length != 2) {
-        valueBytes = default;
-        signatureBytes = default;
-        return false;
-      }
-      else {
-        string valueB64 = parts[0];
-        string signatureB64 = parts[1];
-        valueBytes = BinaryEncoder.Decode(valueB64, encoding);
-        signatureBytes = BinaryEncoder.Decode(signatureB64, encoding);
-        return true;
-      }
-    }
-
-    // Abstract
-    protected abstract byte[] ConvertToBytes(T value);
-    protected abstract T ConvertFromBytes(byte[] data);
   }
 }
